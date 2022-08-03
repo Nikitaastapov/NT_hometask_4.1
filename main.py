d2cd10830f58cdb9be0fb80ca93a5cc2800cc36b
Nikitaastapov <nikitaastapov@gmail.com>
2022-08-03 12:54:35 +0300

    varian_2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open("phonebook_raw.csv") as f:
   contacts_list = list(rows)
 # pprint(contacts_list)
 result=[]
-pattern_1 = r'^(\w+)(\s|,)(\w+)(\s|,)(\w+)?,,?,?'
+pattern_1 = r'^(\w+)(\s|,)(\w+)(\s|,)(\w+)?,{1,3}(\w+)?,(.+)?,(.+)?,(.+)?'
 pattern_2 = r'(\+7|8)(\s)?(\()?(\d{1,3})(\))?(\s|-)?(\d{3})(\s|-)?(\d{2})(\s|-)?(\d{2})(\s)?(\()?(доб)?(\.)?(\s)?(\d+)?(\))?'
 result.append(contacts_list[0])
 result.append(contacts_list[1])
@@ -40,13 +40,16 @@ def AddDubs(list_res, result):
 
 for i in contacts_list[2:]:
     item = ','.join(i)
-    item = res=re.sub(pattern_1, r'\1,\3,\5,', item)
+    item = res=re.sub(pattern_1, r'\1,\3,\5,\6,\7,\8,\9', item)
     res=re.sub(pattern_2, r'+7(\4) \7-\9-\g<11> \g<14>\g<15> \g<17>', item)
+    print('item', item)
     res = res.split(',')
+    print('res', res)
     
     if CheckName(res)=='not':
         result.append(res)
     else:
+        # print(res)
         AddDubs(res,result)
   
 # pprint(result)
